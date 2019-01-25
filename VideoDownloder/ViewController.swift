53ba56f0e70701f98da40150ff397c45ac3aac1b
takaoh717 <hrgc.tko0717@gmail.com>
2019-01-25 21:16:07 +0900

    Initial Commit

---
+++ VideoDownloder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoDownloder
+//
+//  Created by Takao Horiguchi on 2019/01/25.
+//  Copyright © 2019 takaoh717. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ VideoDownloderTests/VideoDownloderTests.swift
@@ -0,0 +1,34 @@
+//
+//  VideoDownloderTests.swift
+//  VideoDownloderTests
+//
+//  Created by Takao Horiguchi on 2019/01/25.
+//  Copyright © 2019 takaoh717. All rights reserved.
+//
+
+import XCTest
+@testable import VideoDownloder
+
+class VideoDownloderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
